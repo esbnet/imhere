98337d8a0dea70af457fa41369039bcb041597ca
esbnet <esbnet@gmail.com>
2023-11-18 10:31:41 -0300

    feat: :sparkles: created form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import {
+  GestureResponderEvent,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+  function handleParticipantAdd(event: GestureResponderEvent): void {
+    alert("oi");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Show do RHCP    </Text>
-      <Text style={styles.eventDate}>Sexta, 30 de junho de 2022  </Text>
-      <StatusBar
-        style="inverted"
-        backgroundColor="transparent"
-        
-      />
+      <Text style={styles.eventName}>Show do RHCP </Text>
+      <Text style={styles.eventDate}>Sexta, 30 de junho de 2022 </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6B6B6B"}
+          keyboardType="numeric"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParticipantAdd}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: "#6B6B6B",
       fontSize: 16,
+    },
+    input: {
+      flex: 1,
+      color: "#FDFCFE",
+      backgroundColor: "#1F1E25",
+      height: 56,
+      borderRadius: 5,
+      padding: 16,
+      fontSize: 16,
+      marginRight: 12,
+    },
+    button: {
+      backgroundColor: "#31CF67",
+      height: 56,
+      width: 56,
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    buttonText: {
+      color: "#FDFCFE",
+      fontSize: 24,
+    },
+    form: {
+      width: "100%",
+      flexDirection: "row",
+      marginTop: 36,
+      marginBottom: 42,
     }
   });
